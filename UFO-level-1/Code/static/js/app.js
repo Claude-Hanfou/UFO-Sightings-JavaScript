aab4bb1e2b35c655181a57625792072a55c2a5bd
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2021-02-14 20:22:27 -0600

    update

--- UFO-level-1/Code/static/js/app.js
+++ UFO-level-1/Code/static/js/app.js
@@ -8,42 +8,6 @@ var tbody = d3.select("tbody");
 // // // // // Console.log the ufo data from data.js
 console.log(data);
 
-// // // // Step 1: Loop Through `data` and console.log each ufo report object
-data.forEach(function(ufoSightings) {
-  console.log(ufoSightings);
-});
-
-// // // // // // Step 2:  Use d3 to append one table row `tr` for ufo report object
-// // // // // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-data.forEach(function(ufoSightings) {
-  console.log(ufoSightings);
-  var row = tbody.append("tr");
-});
-
-// // // // // // Step 3:  Use `Object.entries` to console.log ufo report value
-data.forEach(function(ufoSightings) {
-  console.log(ufoSightings);
-  var row = tbody.append("tr");
-
-  Object.entries(ufoSightings).forEach(function([key, value]) {
-    console.log(key, value);
-  });
-});
-
-// // // // // // Step 4: Use d3 to append 1 cell per ufo report value 
-// data.forEach(function(ufoSightings) {
-//   console.log(ufoSightings);
-//   var row = tbody.append("tr");
-
-//   Object.entries(ufoSightings).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the ufo report object
-//     var cell = row.append("td");
-//   });
-// });
-
-// // // // // // Step 5: Use d3 to update each cell's text with
 // // // // // // ufo report values (date,city,state,country,shape,duration,comments)
 data.forEach(function(ufoSightings) {
   console.log(ufoSightings);
@@ -87,6 +51,8 @@ function updateTable(dataset) {
 // Complete the event handler function for the form
 function runEnter() {
 
+  tbody.html("");
+  
   // Prevent the page from refreshing
   d3.event.preventDefault();
   
@@ -103,14 +69,19 @@ function runEnter() {
 
   console.log(filteredData);
 
-  return filteredData;
+
+filteredData.forEach(function(date) {
+    console.log(date);
+    var row = tbody.append("tr");
+    Object.entries(date).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the ufo report object
+      var cellc = row.append("td");
+      cellc.text(value);
+    });
+  });
+  
 
 }
 
-//use update table function and runEnter function to return filtered data
-
-updateTable(data); 
-button.on("click", function() {
-  var result = runEnter(data);
-  updateTable(result);
-});
--- UFO-level-2/Code/static/js/app.js
+++ UFO-level-2/Code/static/js/app.js
@@ -1,50 +1,14 @@
-// // from data.js
+// // // // // // from data.js
 var tableData = data;
 
-// // // // // YOUR CODE HERE!
-// // // // // // // Get a reference to the table body
+// // // // // // // // // // YOUR CODE HERE!
+// // // // // // // // // // // // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// // // // // Console.log the ufo data from data.js
+// // // // // // // // Console.log the ufo data from data.js
 console.log(data);
 
-// // // // Step 1: Loop Through `data` and console.log each ufo report object
-data.forEach(function(ufoSightings) {
-  console.log(ufoSightings);
-});
-
-// // // // // // Step 2:  Use d3 to append one table row `tr` for ufo report object
-// // // // // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-data.forEach(function(ufoSightings) {
-  console.log(ufoSightings);
-  var row = tbody.append("tr");
-});
-
-// // // // // // Step 3:  Use `Object.entries` to console.log ufo report value
-data.forEach(function(ufoSightings) {
-  console.log(ufoSightings);
-  var row = tbody.append("tr");
-
-  Object.entries(ufoSightings).forEach(function([key, value]) {
-    console.log(key, value);
-  });
-});
-
-// // // // // // Step 4: Use d3 to append 1 cell per ufo report value 
-// data.forEach(function(ufoSightings) {
-//   console.log(ufoSightings);
-//   var row = tbody.append("tr");
-
-//   Object.entries(ufoSightings).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the ufo report object
-//     var cell = row.append("td");
-//   });
-// });
-
-// // // // // // Step 5: Use d3 to update each cell's text with
-// // // // // // ufo report values (date,city,state,country,shape,duration,comments)
+// // // // // // // // // ufo report values (date,city,state,country,shape,duration,comments)
 data.forEach(function(ufoSightings) {
   console.log(ufoSightings);
   var row = tbody.append("tr");
@@ -56,6 +20,7 @@ data.forEach(function(ufoSightings) {
     cell.text(value);
   });
 });
+
 
 
 
@@ -72,65 +37,160 @@ button.on("click", runEnter);
 form.on("submit",runEnter);
 
 
-// Update table with a new dataset by creating a function
-function updateTable(dataset) {
-  tbody.html('');
-  dataset.forEach((toBeDefined) => {
-    var row = tbody.append("tr");
-    Object.entries(toBeDefined).forEach(([key,value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-  });
-}
-
-// Complete the event handler function for the form
+// Complete the event handler function for the form for the date
 function runEnter() {
+
+  tbody.html("");
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
   
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
-  var inputElement2 = d3.select("#city");
-  var inputElement3 = d3.select("#state");
-  var inputElement4 = d3.select("#country");
-  var inputElement5 = d3.select("#shape");
-
-
+  
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
-  var inputValue2 = inputElement2.property("value");
-  var inputValue3 = inputElement3.property("value");
-  var inputValue4 = inputElement4.property("value");
-  var inputValue5 = inputElement5.property("value");
+ 
+  var filteredData1 =  tableData.filter(table => table.datetime === inputValue)
 
+  console.log(filteredData1);
 
-  //console.log(inputValue);
-//   console.log(tableData);
-
-  var filteredData =  tableData.filter(table => table.datetime === inputValue && 
-    table.city === inputValue2  &&
-    table.city === inputValue2 &&
-    table.state === inputValue3 &&
-    table.country === inputValue4 &&
-    table.shape === inputValue5 
-  );
-
-  console.log(filteredData);
-
-  return filteredData;
+  filteredData1.forEach(function(date) {
+    console.log(date);
+    var drow = tbody.append("tr");
+    Object.entries(date).forEach(function([key, value]) {
+      console.log(key, value);
+      var celld = drow.append("td");
+      celld.text(value);
+    });
+  });
 
 }
 
 
-//use update table function and runEnter function to return filtered data
 
-updateTable(data); 
-button.on("click", function() {
-  var result = runEnter(data);
-  updateTable(result);
-});
+// Complete the event handler function for the form for the city
+function runEnter() {
+
+  tbody.html("");
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement2 = d3.select("#city");
+  
+  // Get the value property of the input element
+  var inputValue2 = inputElement2.property("value");
+
+  var filteredData2 =  tableData.filter(table => table.city === inputValue2)
+
+  console.log(filteredData2);
+
+  filteredData2.forEach(function(cit) {
+    console.log(cit);
+    var crow = tbody.append("tr");
+    Object.entries(cit).forEach(function([key, value]) {
+      console.log(key, value);
+      var cellc = crow.append("td");
+      cellc.text(value);
+    });
+  });
+
+}
 
 
+// // // Complete the event handler function for the form for the state
 
+// function runEnter() {
+
+//   tbody.html("");
+
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
+  
+//   // Select the input element and get the raw HTML node
+//   var inputElement3 = d3.select("#state");
+
+//   // Get the value property of the input element
+//   var inputValue3 = inputElement3.property("value");
+
+//   var filteredData3 =  tableData.filter(table => table.state === inputValue3)
+  
+//   console.log(filteredData3);
+
+//   filteredData3.forEach(function(stat) {
+//     console.log(stat);
+//     var row = tbody.append("tr");
+//     Object.entries(stat).forEach(function([key, value]) {
+//       console.log(key, value);
+//       var cells = row.append("td");
+//       cells.text(value);
+//     });
+//   });
+
+// }
+
+
+// // Complete the event handler function for the form for the country
+// function runEnter() {
+
+//   tbody.html("");
+
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
+  
+//   // Select the input element and get the raw HTML node
+//   var inputElement4 = d3.select("#country");
+  
+
+
+//   // Get the value property of the input element
+//   var inputValue4 = inputElement4.property("value");
+
+//   var filteredData4 =  tableData.filter(table => table.country === inputValue4)
+  
+//   console.log(filteredData4);
+
+
+//   filteredData4.forEach(function(count) {
+//     console.log(count);
+//     var row = tbody.append("tr");
+//     Object.entries(count).forEach(function([key, value]) {
+//       console.log(key, value);
+//       var cellc = row.append("td");
+//       cellc.text(value);
+//     });
+//   });
+
+// }
+
+// // Complete the event handler function for the form for the shape
+// function runEnter() {
+
+//   tbody.html("");
+
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
+  
+//   // Select the input element and get the raw HTML node
+//   var inputElement5 = d3.select("#shape");
+  
+//   // Get the value property of the input element
+//   var inputValue5 = inputElement5.property("value");
+
+//   var filteredData5 =  tableData.filter(table => table.shape === inputValue5)
+ 
+//   console.log(filteredData5);
+
+//   filteredData5.forEach(function(scit) {
+//     console.log(scit);
+//     var row = tbody.append("tr");
+//     Object.entries(scit).forEach(function([key, value]) {
+//       console.log(key, value);
+//       var cellc = row.append("td");
+//       cellc.text(value);
+//     });
+//   });
+
+// }
